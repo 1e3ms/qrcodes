765d2c29338483a7e8632af5cb5876dd8dc65af2
Joao Eduardo Luis <joao@1e3ms.io>
2024-02-19 16:28:07 +0000

    frontend: make it prettier
    Signed-off-by: Joao Eduardo Luis <joao@1e3ms.io>

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { QrcodesComponent } from './qrcodes/qrcodes.component';
+import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
 
 @Component({
   selector: 'onesec-root',
   standalone: true,
-  imports: [RouterOutlet, QrcodesComponent],
+  imports: [RouterOutlet, QrcodesComponent, FontAwesomeModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = '1e3ms-qrcodes';
+  constructor(library: FaIconLibrary) {
+    library.addIconPacks(fas, far);
+  }
 }
--- frontend/src/app/qrcodes/qrcodes.component.ts
+++ frontend/src/app/qrcodes/qrcodes.component.ts
@@ -1,24 +1,29 @@
 import { Component, Pipe, PipeTransform, SecurityContext } from '@angular/core';
 import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
-import QRCode from "qrcode";
+import QRCode from 'qrcode';
 import Konva from 'konva';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { DomSanitizer, SafeValue } from '@angular/platform-browser';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 type CheckURLResponse = {
   accessible?: boolean;
   found?: boolean;
-}
+};
 
-@Pipe(
-  { name: 'safe', standalone: true }
-)
+type QRCodeError = {
+  notFound?: boolean;
+  invalidURL?: boolean;
+  notSecureURL?: boolean;
+};
+
+@Pipe({ name: 'safe', standalone: true })
 export class SafePipe implements PipeTransform {
-  constructor(private domSanitizer: DomSanitizer) { }
+  constructor(private domSanitizer: DomSanitizer) {}
   transform(url: string | null | SafeValue) {
     if (url === null) {
-      return "";
+      return '';
     }
     return this.domSanitizer.sanitize(SecurityContext.URL, url);
   }
@@ -27,69 +32,89 @@ export class SafePipe implements PipeTransform {
 @Component({
   selector: 'onesec-qrcodes',
   standalone: true,
-  imports: [ReactiveFormsModule, HttpClientModule, CommonModule, SafePipe],
+  imports: [ReactiveFormsModule, HttpClientModule, CommonModule, SafePipe, FontAwesomeModule],
   templateUrl: './qrcodes.component.html',
   styleUrl: './qrcodes.component.scss'
 })
 export class QrcodesComponent {
+  public qrcodeURL = new FormControl('', Validators.required);
 
-  public qrcodeURL = new FormControl("", Validators.required);
+  public error?: QRCodeError;
+  public invalidURL?: boolean;
+  public submitted: boolean = false;
 
-  public constructor(private http: HttpClient, private domSanitizer: DomSanitizer) { }
+  public constructor(
+    private http: HttpClient,
+    private domSanitizer: DomSanitizer
+  ) {}
 
   public submitURL() {
     const url = this.qrcodeURL.value;
 
-    console.log("submit! invalid: ", this.qrcodeURL.invalid);
+    this.submitted = true;
+    console.log('submit! invalid: ', this.qrcodeURL.invalid);
+
+    this.error = undefined;
+    this.invalidURL = undefined;
 
     if (url === null || url === undefined || this.qrcodeURL.invalid) {
+      this.error = { invalidURL: true };
+      this.invalidURL = true;
       return;
-    } else if (!url.startsWith("https://")) {
+    } else if (!url.startsWith('https://')) {
+      this.error = { notSecureURL: true };
+      this.invalidURL = true;
       // error
       return;
     }
 
     // check url exists
-    this.http.post<CheckURLResponse>("/api/check", { url: url }).subscribe(
-      (res: CheckURLResponse) => {
-        console.debug("valid url: ", res.found, res.accessible);
-      }
-    );
+    this.http
+      .post<CheckURLResponse>('/api/check', { url: url })
+      .subscribe((res: CheckURLResponse) => {
+        console.debug('valid url: ', res.found, res.accessible);
 
-    this.generateKonva(url);
+        if (res.found === false) {
+          this.error = { notFound: true };
+          this.invalidURL = true;
+        } else {
+          this.invalidURL = false;
+          this.generateKonva(url);
+        }
+      });
   }
 
   private generateKonva(url: string) {
     const stage = new Konva.Stage({
-      container: "konva-container",
+      container: 'konva-container',
       width: 500,
-      height: 800,
+      height: 800
     });
 
     const layer = new Konva.Layer();
 
     const rect = new Konva.Rect({
-      stroke: "black",
-      fill: "blue",
+      stroke: 'black',
+      fill: 'blue',
       height: 800,
-      width: 500,
+      width: 500
     });
 
     const headerText = new Konva.Text({
       y: 60,
-      text: "Please find the relevant documents at\n",
+      text: 'Please find the relevant documents at\n',
       fontSize: 16,
       width: 500,
-      fill: "black",
-      stroke: "white",
-      fontFamily: "Calibri",
-      align: "center",
-    })
+      fill: 'black',
+      stroke: 'white',
+      fontFamily: 'Calibri',
+      align: 'center'
+    });
 
-    const cv = document.createElement("canvas");
+    const cv = document.createElement('canvas');
     QRCode.toCanvas(cv, url, {
       width: 300,
-      margin: 2,
+      margin: 2
     });
 
     const img = new Konva.Image({
@@ -97,13 +122,12 @@ export class QrcodesComponent {
       width: 300,
       height: 300,
       y: 100,
-      x: 20,
-    })
+      x: 20
+    });
 
     layer.add(rect);
     layer.add(headerText);
     layer.add(img);
     stage.add(layer);
   }
-
 }
